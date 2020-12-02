c25357177ba9039ca379d925dd1f8f2ef9f78621
Colin Cooper <evinruding@yahoo.com>
2020-12-01 20:36:59 -0500

    initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# Sitecore-10-k8s-deploy-scripts
- 
+# Colin Coopers "All in one" script for deploying non-production Sitecore 10 "XM" into AKS
+
+## Installing prerequisite software
+
+- [Install Powershell](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7)
+- [Install the Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli) / [Current release of the Azure CLI](https://aka.ms/installazurecliwindows)
+- [Install Chocolatey]
+`Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))`
+
+- [Install Helm]
+`choco install kubernetes-helm`
+
+- [Install kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-windows)
+
+
+This all in one script will install [Kubernetes](https://kubernetes.io) version ```1.18.10```. If you want to change to another version, please update the script.
+
+``` 
+# list Azure locations
+az account list-locations -o table
+
+# get  Azure locations
+az aks get-versions -l east-us-2 -o table 
+
+KubernetesVersion    Upgrades
+-------------------  -----------------------------------------
+1.19.3(preview)      None available
+1.19.0(preview)      1.19.3(preview)
+1.18.10              1.19.0(preview), 1.19.3(preview)
+1.18.8               1.18.10, 1.19.0(preview), 1.19.3(preview)
+1.17.13              1.18.8, 1.18.10
+1.17.11              1.17.13, 1.18.8, 1.18.10
+1.16.15              1.17.11, 1.17.13
+1.16.13              1.16.15, 1.17.11, 1.17.13
+```
+
+## Script Notes
+
+All the scripts has ```default``` values in place.
+
+***Please CD into the directory on your workstation that contains the  k8s-sitecore-xm1 folder and execute the script from there***
+
+In addition to the prerequisites listed you will need a valid Sitecore License File.
+The license.xml issued by Sitecore needs to be compressed/encoded into the sitecore-license.txt file in the secrets folder.
+A helper script can be found in the official guide which converts the license.xml into this format.
+Look on [Page 20 of the official installation guide for this helper script](https://dev.sitecore.net/~/media/D6D6C46E2A89478D92CA10BCDD19BBEF.ashx)
+
+SSL:
+I would reccomend generating a wildcard SSL certificate for your test/dev deployments.
+In my own testing I have used the free "Lets Encrypt" wildcard certificate to great success.
+An excellent guide for getting this done in a windows environment exists [here](https://medium.com/@nvbach91/how-to-create-lets-encrypt-s-free-wildcard-ssl-certificates-for-windows-server-iis-web-servers-aa01d939e0ad0 
+
+Each of the following folders Requires 2 certificate files (tls.crt and tls.key) :
+k8s-sitecore-xm1\secrets\tls\global-cd
+k8s-sitecore-xm1\secrets\tls\global-cm
+k8s-sitecore-xm1\secrets\tls\global-id
+
+The following secret files need to be modified with passwords. Make sure that there are no empty lines.
+sitecore-core-database-password.txt
+sitecore-core-database-username.txt
+
+sitecore-databasepassword.txt
+sitecore-databaseusername.txt
+
+sitecore-web-database-password.txt
+sitecore-web-database-username.txt
+
+sitecore-master-database-password.txt
+sitecore-master-database-username.txt
+
+sitecore-forms-database-password.txt
+sitecore-forms-database-username.txt
+
+Be patient, some steps can take a little time to complete.
+You may need to adjust the sleep settings in the script to compensate for differences in execution times in different azure regions
+Some Helm/Nginx Errors are expected, these are safe to ignore.
+
+Many thanks to Bart Plasmeijer and Rob Earlam For laying the framework for this at Symposium 2020.
---
+++ Sc10-XM-K8S--Azure-Dev-Deploy.ps1
@@ -0,0 +1,175 @@
+﻿<#
+.SYNOPSIS
+    This script builds a "XM" Sitecore 10 non production Azure Kubernetes deployment
+    into an azure resource group of your choice. 
+
+.DESCRIPTION
+    This is an "all in one" script for deploying Sitecore 10 into AKS
+
+.NOTES
+    Author: Colin Cooper
+    Last Edit: 2020-12-1
+    Version 1.5 Beta - initial release of Sc10 Azure test Deployment Script
+
+***Please CD into the directory that contains the  k8s-sitecore-xm1 folder and execute the script from there***
+Be patient, some steps can take a little time to complete.
+
+Prerequisites:
+
+Some Helm/Nginx Errors are expected, these are safe to ignore
+
+Install Azure CLI
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
+Install powershell 7
+msiexec.exe /package PowerShell-7.1.0-win-x64.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+
+#>
+
+
+
+Write-Host "--- Setting up CLI & Params ---" -ForegroundColor yellow 
+$azsubname = 'Your-Azure-Sub-Name' #The name of the target azure sub
+$Region = 'eastus2' #location of all Azure resources
+$ResourceGroup = 'SC10K8Dev1' # the resource group for Azure Kubernetes
+$AcrName = 'SC10K8Dev1'
+$SkuAcr = 'Standard'  #the Sku Type of the Azure Container Registry
+$AKSname = 'SC10K8DevAKS1' #the name of your Azure Kubernetes cluster
+$aksVersion = "1.18.10" #$(az aks get-versions -l $Region --query 'orchestrators[-1].orchestratorVersion' -o tsv)
+$AzureWindowsUser = 'azureuser'
+$AzureWindowsPassword="Password!12345" #change this to something secure
+$namespace = "sitecore"
+Write-Host "--- Complete: CLI & Params Configured ---" -ForegroundColor Green
+
+
+# Create resource group
+Write-Host "--- Creating resource group ---" -ForegroundColor Yellow
+az group create --name $ResourceGroup --location $Region
+Write-Host "--- Complete: resource group ---" -ForegroundColor Green
+
+# Create Azure Container Registry
+Write-Host "--- Creating ACR ---" -ForegroundColor Yellow
+az acr create -n $AcrName -g $ResourceGroup --sku $SkuAcr --location $Region
+Write-Host "--- Complete: ACR ---" -ForegroundColor Green
+
+# Setup CLI & Parameters for AKS creation
+Write-Host "--- Setting up CLI & Params ---" -ForegroundColor yellow
+
+$aksVersion = "1.18.8" #$(az aks get-versions -l $Region --query 'orchestrators[-1].orchestratorVersion' -o tsv)
+Write-Host "--- Complete: CLI & Params Configured ---" -ForegroundColor Green
+
+# create AKS instance
+Write-Host "--- Creating AKS Instance K8s version $aksVersion ---" -ForegroundColor yellow
+az aks create `
+    --name $AksName `
+    --resource-group $ResourceGroup `
+    --kubernetes-version $aksVersion `
+    --node-count 1 `
+    --node-vm-size Standard_D4s_v3 `
+    --vm-set-type VirtualMachineScaleSets `
+    --generate-ssh-keys `
+    --load-balancer-sku standard `
+    --network-plugin azure `
+    --node-osdisk-size 128 `
+    --windows-admin-password $AzureWindowsPassword `
+    --windows-admin-username $AzureWindowsUser `
+	--enable-addons monitoring `
+	--nodepool-name 'linux' `
+    --verbose
+
+sleep 30
+
+Write-Host "--- Complete: AKS Created ---" -ForegroundColor Green
+
+# Get the Creds Boy-o
+az aks get-credentials --resource-group $ResourceGroup --name $Aksname --overwrite-existing
+
+# link AKS to ACR
+Write-Host "--- Linking AKS to ACR ---" -ForegroundColor yellow
+$clientID = $(az aks show --resource-group $ResourceGroup --name $AksName --query "servicePrincipalProfile.clientId" --output tsv)
+$acrId = $(az acr show --name $AcrName --resource-group $ResourceGroup --query "id" --output tsv)
+az role assignment create --assignee $clientID `
+    --role acrpull `
+    --scope $acrId
+Write-Host "--- Complete: AKS & ACR Linked ---" -ForegroundColor Green
+
+sleep 20
+
+# Add windows server nodepool
+Write-Host "--- Creating Windows Server Node Pool ---" -ForegroundColor yellow
+az aks nodepool add --resource-group $ResourceGroup `
+    --cluster-name $AksName `
+    --os-type Windows `
+    --name win `
+    --node-vm-size Standard_D4s_v3 `
+    --node-count 1 
+Write-Host "--- Complete: Windows Server Node Pool Created ---" -ForegroundColor Green
+
+sleep 30
+
+# authenticate AKS instance
+Write-Host "--- Authenticate with AKS ---" -ForegroundColor yellow
+az aks get-credentials -a `
+    --resource-group $ResourceGroup `
+    --name $AksName `
+    --overwrite-existing
+Write-Host "--- Complete: Windows Server Node Pool Created ---" -ForegroundColor Green
+
+Sleep 100
+
+Write-Host "--- Creating nginx (Ingress) ---" -ForegroundColor Yellow
+# kubectl create namespace ingress-basic 
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo update
+helm install nginx-ingress stable/nginx-ingress --set controller.replicaCount=1 --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux --set-string controller.config.proxy-body-size=10m --set controller.service.externalTrafficPolicy=Local --debug
+
+Write-Host "--- Complete: nginx (ingress) ---" -ForegroundColor Green
+
+sleep 30
+
+
+# Show the Public IP of the NginX Ingress Controller
+$ingressIpAddress = (kubectl get svc nginx-ingress-controller -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+
+
+if (-not [string]::IsNullOrEmpty($ingressIpAddress)) {
+  Write-Host "Ingress Public IP: $ingressIpAddress" -ForegroundColor Yellow
+}
+
+
+Write-Host "--- Complete: nginx (ingress) ---" -ForegroundColor Green
+
+Write-Host "--- Deploy Secrets ---" -ForegroundColor Yellow
+kubectl apply -k ./k8s-sitecore-xm1/secrets/
+Write-Host "--- Complete: Deploy Secrets ---" -ForegroundColor Green
+
+Write-Host "--- Deploy External Resources ---" -ForegroundColor Yellow
+kubectl apply -f ./k8s-sitecore-xm1/external/
+kubectl wait --for=condition=Available deployments --all --timeout=900s
+kubectl wait --for=condition=Ready pods --all
+Write-Host "--- Complete: Deploy External Resources ---" -ForegroundColor Green
+
+Write-Host "--- Initialize SQl and SOLR ---" -ForegroundColor Yellow
+kubectl apply -f ./k8s-sitecore-xm1/init/
+kubectl wait --for=condition=Complete job.batch/solr-init --timeout=600s
+kubectl wait --for=condition=Complete job.batch/mssql-init --timeout=600sWrite-Host "--- Complete: Initialize SQl and SOLR ---" -ForegroundColor Green
+
+Write-Host "--- Deploy Sitecore ---" -ForegroundColor Yellow
+kubectl apply -f ./k8s-sitecore-xm1/
+kubectl wait --for=condition=Available deployments --all --timeout=36000s
+
+kubectl apply -f ./k8s-sitecore-xm1/ingress-nginx
+kubectl wait --for=condition=Available deployments --all --timeout=36000s
+Write-Host "--- Complete: Deploy Sitecore ---" -ForegroundColor Green
+
+Write-Host "--- Sitecore Azure Containers Deployment complete  ---" -ForegroundColor Green
+Write-Host "--- Update the local host file with the external IP address Below  ---" -ForegroundColor Green
+Write-Host "--- The default hostnames are cm.globalhost cd.globalhost Id.globalhost  ---" -ForegroundColor Green
+
+if (-not [string]::IsNullOrEmpty($ingressIpAddress)) {
+  Write-Host "Ingress Public IP: $ingressIpAddress" -ForegroundColor Yellow
+}
+
+Write-Host "--- When the deployment is finished, you must configure the SolrCloud search indexes.  ---" -ForegroundColor Green
+Write-Host "--- Login to Sitecore CMS with the admin user and password that you configured as a secret  ---" -ForegroundColor Green
+Write-Host "--- In the Sitecore Control Panel click Populate Managed Schema and in the Schema Populate dialog box, select all the indexes and then click Populate. ---" -ForegroundColor Green
